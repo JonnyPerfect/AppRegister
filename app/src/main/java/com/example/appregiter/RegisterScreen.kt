0beb32f729153be662ff04141fcc695f3a959b36
King <kurvanovjurat@gmail.com>
2024-06-08 17:13:50 +0600

    создание приложение

---
+++ app/src/main/java/com/example/appregiter/RegisterScreen.kt
@@ -0,0 +1,12 @@
+package com.example.appregiter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_screen)
+    }
+}
